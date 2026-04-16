ab0f6f78ae16ed7aeb10d8b3034d643e25b3c558
Vinicius Tonetti <vrtonetti@gmail.com>
2026-04-16 19:08:53 -0300

    iniciando código extração variáveis paisagens

---
+++ variaveis_paisagem.R
@@ -0,0 +1,11 @@
+# Extraindo vaiárveis paisagem para coordenadas específicas
+
+
+# pacotes e limpando ambiente --------------------------------------------------
+
+rm(list = ls())
+
+#remotes::install_github("mauriciovancine/atlanticr")
+
+library(terra)
+library(atlanticr)
