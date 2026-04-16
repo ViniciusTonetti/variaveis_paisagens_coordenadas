d8392bbbd3711c6526f178eae012a828cdfd598f
Vinicius Tonetti <vrtonetti@gmail.com>
2026-04-16 20:14:53 -0300

    fazendo os buffers

--- variaveis_paisagem.R
+++ variaveis_paisagem.R
@@ -9,3 +9,28 @@ rm(list = ls())
 
 library(terra)
 library(atlanticr)
+
+unique(atlanticr::atlantic_spatial$metric_group)
+
+
+
+# pontos e buffers -------------------------------------------------------------
+
+pts <- terra::vect("E:/_PESSOAL/ViniciusT/variaveis paisagem coordenadas/pontos/pts_paisagens.shp")
+
+
+# cobvertendo para sirgas para criar os buffers
+pts_sirgas <- terra::project(pts, "EPSG:5641")
+
+# criando os buffers de 2 km
+
+buf_500m <- terra::buffer(pts_sirgas, width = 500)
+buf_1km <- terra::buffer(pts_sirgas, width = 1000)
+buf_2km <- terra::buffer(pts_sirgas, width = 2000)
+buf_3km <- terra::buffer(pts_sirgas, width = 3000)
+buf_5km <- terra::buffer(pts_sirgas, width = 5000)
+
+
+
+
+
