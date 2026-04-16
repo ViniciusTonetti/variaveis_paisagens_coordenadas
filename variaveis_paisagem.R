f49b4dcc3459c9028aa27163ab70d612d0957792
Vinicius Tonetti <vrtonetti@gmail.com>
2026-04-16 20:44:31 -0300

    importando e convertendo mapbiomas

--- variaveis_paisagem.R
+++ variaveis_paisagem.R
@@ -19,7 +19,7 @@ unique(atlanticr::atlantic_spatial$metric_group)
 pts <- terra::vect("E:/_PESSOAL/ViniciusT/variaveis paisagem coordenadas/pontos/pts_paisagens.shp")
 
 
-# cobvertendo para sirgas para criar os buffers
+# convertendopara sirgas para criar os buffers
 pts_sirgas <- terra::project(pts, "EPSG:5641")
 
 # criando os buffers de 2 km
@@ -31,15 +31,16 @@ buf_3km <- terra::buffer(pts_sirgas, width = 3000)
 buf_5km <- terra::buffer(pts_sirgas, width = 5000)
 
 
-# Convertendo os buffers novamente para WGS
-
-buf_500m <- terra::project(buf_500m, 'EPSG:4326')
-buf_1km <- terra::project(buf_1km, 'EPSG:4326')
-buf_2km <- terra::project(buf_2km, 'EPSG:4326')
-buf_3km <- terra::project(buf_3km, 'EPSG:4326')
-buf_5km <- terra::project(buf_5km, 'EPSG:4326')
 
 
+# Cobertura da terra ano 2022 coleção 10 MapBiomas -----------------------------
 
+mb_br_22 <- terra::rast("E:/_PESSOAL/ViniciusT/variaveis paisagem coordenadas/mapbiomas/brazil_coverage_2022.tif")
 
+mb_br_22_SIRGAS <- project(mb_br_22, "EPSG:5641", method = "near")
+
+output <- "E:/_PESSOAL/ViniciusT/variaveis paisagem coordenadas/mapbiomas/"
+  
+#writeRaster(mb_br_22_SIRGAS, paste0(output, "clay_fraction_0_10.tif"),     
+#            gdal=c("COMPRESS=DEFLATE", "TFW=YES"), overwrite = T)
 
